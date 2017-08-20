b407338849b1497183dbb46f8d6a8d28d2fcf6b6
Filipe <filipe.mudadu@gmail.com>
2017-08-20 12:56:14 -0300

    Normalização de versão e adição de pulo
    Normalização da versão do unity para 2017.1.0f3
    Adção de mecanicas basicas de movimentação.

---
+++ Somnia/Assets/Sandbox/Filipe/PlayerMotor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controle;
+    private Vector3 moveVector = Vector3.zero;
+
+    public float verticalVel;
+    public float gravidade = 120.0f;
+    public float puloForca = 80.0f;
+
+    // Use this for initialization
+    private void Start ()
+    {
+        controle = GetComponent<CharacterController>();
+
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+	    if(controle.isGrounded)
+        {
+            verticalVel = -gravidade * Time.deltaTime;
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVel = puloForca;
+            }
+        }
+        else
+        {
+            verticalVel -= gravidade * Time.deltaTime;
+        }
+
+        
+        moveVector.x = Input.GetAxis("Horizontal") * 150.0f;
+        moveVector.y = verticalVel;
+        moveVector.z = Input.GetAxis("Vertical") * 150.0f;
+        controle.Move(moveVector * Time.deltaTime);
+
+
+    }
+}
